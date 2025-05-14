1cf16f22c27f31324ee17eeddb34be39ac01772c
bak3839 <bak3839@naver.com>
2025-05-15 00:34:10 +0900

    조회수 측정 및 백업 API 테스트

---
+++ service/view/src/test/kotlin/kuke/board/view/api/ViewApiTest.kt
@@ -0,0 +1,37 @@
+package kuke.board.view.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.web.client.RestClient
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+
+class ViewApiTest {
+    val restClient = RestClient.create("http://localhost:9003")
+
+    @Test
+    fun viewTest() {
+        val executors = Executors.newFixedThreadPool(100)
+        val latch = CountDownLatch(10000)
+
+        for(i in 1..10000) {
+            executors.submit {
+                restClient.post()
+                    .uri("/v1/article-views/articles/1/users/1")
+                    .retrieve()
+
+                latch.countDown()
+            }
+        }
+
+        latch.await()
+
+        val count = restClient.get()
+            .uri("/v1/article-views/articles/1/count")
+            .retrieve()
+            .body(Long::class.java)
+
+        println("count: $count")
+        executors.shutdown()
+    }
+}
