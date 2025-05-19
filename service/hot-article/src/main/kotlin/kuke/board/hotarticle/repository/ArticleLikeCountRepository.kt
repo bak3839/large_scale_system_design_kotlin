b8653912121ac876b224c608910d951d4c2a6325
bak3839 <bak3839@naver.com>
2025-05-19 21:39:42 +0900

    게시글 좋아요 수 저장하는 이벤트 리포지토리 구현

---
+++ service/hot-article/src/main/kotlin/kuke/board/hotarticle/repository/ArticleLikeCountRepository.kt
@@ -0,0 +1,23 @@
+package kuke.board.hotarticle.repository
+
+import org.springframework.data.redis.core.StringRedisTemplate
+import java.time.Duration
+import java.time.format.DateTimeFormatter
+
+class ArticleLikeCountRepository(
+    private val redisTemplate: StringRedisTemplate
+) {
+    companion object {
+        private val KEY_FORMAT = "hot-article::article::%s::like-count"
+    }
+
+    fun createOrUpdate(articleId: Long, likeCount: Long, ttl:Duration) {
+        redisTemplate.opsForValue().set(generateKey(articleId), likeCount.toString(), ttl)
+    }
+
+    fun read(articleId: Long) : Long
+    = redisTemplate.opsForValue().get(generateKey(articleId))?.toLong() ?: 0L
+
+    private fun generateKey(articleId: Long): String
+    = KEY_FORMAT.format(articleId)
+}
