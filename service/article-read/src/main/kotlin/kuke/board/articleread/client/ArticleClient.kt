279d273b830fe4d4743d5e218c8950ceaf9d61d8
bak3839 <bak3839@naver.com>
2025-06-03 23:17:08 +0900

    게시글 원본 데이터 조회를 위한 클라이언트 구현

--- service/article-read/src/main/kotlin/kuke/board/articleread/client/ArticleClient.kt
+++ service/article-read/src/main/kotlin/kuke/board/articleread/client/ArticleClient.kt
@@ -3,6 +3,7 @@ package kuke.board.articleread.client
 import jakarta.annotation.PostConstruct
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
 import java.time.LocalDateTime
@@ -34,6 +35,46 @@ class ArticleClient {
         }
     }
 
+    fun readAll(boardId: Long, page: Long, pageSize: Long): ArticlePageResponse? {
+        try {
+            return restClient.get()
+                .uri("/v1/articles/all?boardId=$boardId&page=$page&pageSize=$pageSize")
+                .retrieve()
+                .body(ArticlePageResponse::class.java)
+        } catch (e: Exception) {
+            log.error(e) { "[ArticleClient.readAll] boardId=$boardId, page=$page, pageSize=$pageSize" }
+            return ArticlePageResponse.EMPTY
+        }
+    }
+
+    fun readAllInfiniteScroll(boardId: Long, lastArticleId: Long?, pageSize: Long): List<ArticleResponse>? {
+        try {
+            return restClient.get()
+                .uri(
+                    lastArticleId?.let {
+                        "/v1/articles/infinite-scroll?boardId=$boardId&lastArticleId=$lastArticleId&pageSize=$pageSize"
+                    } ?: "/v1/articles/infinite-scroll?boardId=$boardId&pageSize=$pageSize"
+                )
+                .retrieve()
+                .body(object : ParameterizedTypeReference<List<ArticleResponse>>() {})
+        } catch (e: Exception) {
+            log.error(e) {"[ArticleClient.readAllInfiniteScroll] boardId=$boardId lastArticleId=$lastArticleId pageSize=$pageSize" }
+            return emptyList()
+        }
+    }
+
+    fun count(boardId: Long): Long {
+        try {
+            return restClient.get()
+                .uri("/v1/articles/boards/$boardId/count")
+                .retrieve()
+                .body(Long::class.java) ?: 0L
+        } catch (e: Exception) {
+            log.error(e) {"[ArticleClient.count] boardId=$boardId"}
+            return 0L
+        }
+    }
+
     companion object {
         data class ArticleResponse(
             var articleId: Long,
@@ -44,5 +85,14 @@ class ArticleClient {
             var createdAt: LocalDateTime = LocalDateTime.now(),
             var modifiedAt: LocalDateTime = LocalDateTime.now(),
         )
+
+        data class ArticlePageResponse(
+            val articles: List<ArticleResponse>,
+            val articleCount: Long
+        ) {
+            companion object {
+                val EMPTY = ArticlePageResponse(listOf(), 0L)
+            }
+        }
     }
 }
