5fb822bba473ab1c198ca20dad24472324350fb3
bak3839 <bak3839@naver.com>
2025-06-05 01:21:23 +0900

    게시글 Query 서버 조회 테스트

--- service/article-read/src/test/kotlin/kuke/board/articleread/api/ArticleReadApiTest.kt
+++ service/article-read/src/test/kotlin/kuke/board/articleread/api/ArticleReadApiTest.kt
@@ -1,19 +1,60 @@
 package kuke.board.articleread.api
 
+import kuke.board.articleread.service.response.ArticleReadPageResponse
 import kuke.board.articleread.service.response.ArticleReadResponse
 import org.junit.jupiter.api.Test
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.web.client.RestClient
 
 class ArticleReadApiTest {
-    val restClient: RestClient = RestClient.create("http://localhost:9005")
+    val articleReadRestClient: RestClient = RestClient.create("http://localhost:9005")
+    val articleRestClient: RestClient = RestClient.create("http://localhost:9000")
 
     @Test
     fun readTest() {
-        val response = restClient.get()
+        val response = articleReadRestClient.get()
             .uri("/v1/articles/${177342987480424448}")
             .retrieve()
             .body(ArticleReadResponse::class.java)
 
         println("response = ${response}")
+    }
+
+    @Test
+    fun readAllTest() {
+        val response1 = articleReadRestClient.get()
+            .uri("/v1/articles?boardId=1&page=1&pageSize=5")
+            .retrieve()
+            .body(ArticleReadPageResponse::class.java)
+
+        println("response1.getArticleCount() = ${response1?.articleCount}")
+
+        response1?.articles?.forEach { println("article.getArticleId() = ${it.articleId}") }
+
+        val response2 = articleRestClient.get()
+            .uri("/v1/articles/all?boardId=1&page=1&pageSize=5")
+            .retrieve()
+            .body(ArticleReadPageResponse::class.java)
+
+        println("response1.getArticleCount() = ${response2?.articleCount}")
+
+        response2?.articles?.forEach { println("article.getArticleId() = ${it.articleId}") }
+    }
+
+    @Test
+    fun readAllInfiniteScrollTest() {
+        val response1 = articleReadRestClient.get()
+            .uri("/v1/articles/infinite-scroll?boardId=1&pageSize=5")
+            .retrieve()
+            .body(object : ParameterizedTypeReference<List<ArticleReadResponse>>() {})
+
+        response1?.forEach { println("response = ${it.articleId}") }
+
+        val response2 = articleRestClient.get()
+            .uri("/v1/articles/infinite-scroll?boardId=1&lastArticleId=&pageSize=5")
+            .retrieve()
+            .body(object : ParameterizedTypeReference<List<ArticleReadResponse>>() {})
+
+        response2?.forEach { println("response = ${it.articleId}") }
     }
 }
